9a2937f8520d452e84b27c0ab6e6f28f34247f91
Neal Striegler-Pettersson <strigs@gmail.com>
2019-10-17 12:44:17 -0400

    run 2to3 script

--- cachemodel/models.py
+++ cachemodel/models.py
@@ -20,6 +20,7 @@ from cachemodel import CACHE_FOREVER_TIMEOUT
 from cachemodel.managers import CacheModelManager, CachedTableManager
 from cachemodel.decorators import find_fields_decorated_with
 from cachemodel.utils import generate_cache_key
+import collections
 
 
 
@@ -84,7 +85,7 @@ class CacheModel(models.Model):
         if not getattr(method, '_cached_method', False):
             raise AttributeError("method '%s' is not a cached_method.");
         target = getattr(method, '_cached_method_target', None)
-        if callable(target):
+        if isinstance(target, collections.Callable):
             key = generate_cache_key([self.__class__.__name__, target.__name__, self.pk], *args, **kwargs)
             data = target(self, *args, **kwargs)
             cache.set(key, data, CACHE_FOREVER_TIMEOUT)
